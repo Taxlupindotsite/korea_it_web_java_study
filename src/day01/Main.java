4c3b558ddbaea07dee78b235ac79248b0cb611bb
Taxlupindotsite <kookoo4186@gmail.com>
2025-05-13 21:46:06 +0900

    init: first commit

--- src/day01/Main.java
+++ src/day01/Main.java
@@ -3,5 +3,24 @@ package day01;
 public class Main {
     public static void main(String[] args) {
 //        System.out.println("hello"); sout , ctrl+/ (주석처리)
+        System.out.println("world");
     }
 }
+
+/*
+깃허브 등록
+
+Terminal
+
+git init
+git status
+git add .
+git commit -m "init: 초기화"
+
+wq(초기로)
+
+1. (등록)
+git config --local user.name '(깃허브 이름)'
+git config --local user.email '(깃허브 이메일)'
+
+ */
