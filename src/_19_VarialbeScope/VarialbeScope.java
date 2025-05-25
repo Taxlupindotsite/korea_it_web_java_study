aaf3c33ede57e5879146e6549933e87688816d87
Taxlupindotsite <kookoo4186@gmail.com>
2025-05-26 06:47:26 +0900

    VariableScope

---
+++ src/_19_VarialbeScope/VarialbeScope.java
@@ -0,0 +1,29 @@
+package _19_VarialbeScope;
+
+public sealed class VarialbeScope{
+
+        //지역변수와 전역변수
+
+
+public static void methodA() {
+    String str = "abc";
+//        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+
+        String name = "구영현";
+
+//        System.out.println(str);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+//        System.out.println(i);
+
+        {int age = 27}
+//        System.out.println(age);
+
+
+    }
+}
